2d6f6d7a19e1e13683952ccc422a2754bfea4398
Mikhail.Kochetov <mikhail.kochetov@gmail.com>
2021-11-30 19:40:14 +0300

    Commit_1

---
+++ script.js
@@ -0,0 +1,18 @@
+"use strict";
+
+//const result = confirm("Are you here?");
+//console.log(result);
+
+// const answers = [];
+
+// answers [0] = prompt ('Как Ваше имя?', '');
+// answers [1] = prompt ('Как Ваша фамилия?', '');
+// answers [2] = prompt('Сколько Вам лет?', '');
+
+// document.write(answers);
+
+// const category = 'toys';
+// console.log(`https://someurl.com/${category}/5`);
+
+const user = 'Михаил!';
+alert(`Привет, ${user}`);
